636dbc7c77539676486faf61f4533c61e5d175e2
Wojciech <swirjestem@hotmail.com>
2026-09-19 19:29:20 +0200

    Configure SCRAP DASH Unity player settings automatically

---
+++ games/001_scrap_dash/Assets/Editor/ScrapDashProjectSetup.cs
@@ -0,0 +1,41 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace ScrapDash.Editor
+{
+    [InitializeOnLoad]
+    public static class ScrapDashProjectSetup
+    {
+        static ScrapDashProjectSetup()
+        {
+            EditorApplication.delayCall += Apply;
+        }
+
+        public static void Apply()
+        {
+            PlayerSettings.companyName = "SWIR";
+            PlayerSettings.productName = "SCRAP DASH";
+            PlayerSettings.bundleVersion = "0.1.0";
+            PlayerSettings.defaultScreenWidth = 1920;
+            PlayerSettings.defaultScreenHeight = 1080;
+            PlayerSettings.fullScreenMode = FullScreenMode.FullScreenWindow;
+            PlayerSettings.resizableWindow = true;
+            PlayerSettings.runInBackground = false;
+
+            var settingsAssets = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/ProjectSettings.asset");
+            if (settingsAssets.Length == 0) return;
+
+            var serialized = new SerializedObject(settingsAssets[0]);
+            var activeInputHandler = serialized.FindProperty("activeInputHandler");
+            if (activeInputHandler != null && activeInputHandler.intValue != 1)
+            {
+                activeInputHandler.intValue = 1;
+                serialized.ApplyModifiedPropertiesWithoutUndo();
+                AssetDatabase.SaveAssets();
+                Debug.Log("SCRAP DASH: enabled the new Unity Input System backend.");
+            }
+        }
+    }
+}
+#endif
