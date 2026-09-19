fd1d45ae1a363ecc979acf8824848b02199c8bef
Wojciech <swirjestem@hotmail.com>
2026-09-19 19:25:53 +0200

    Define SCRAP DASH Level 1 acceptance contract

---
+++ games/001_scrap_dash/Assets/Scripts/Runtime/LevelDefinition.cs
@@ -0,0 +1,25 @@
+namespace ScrapDash
+{
+    public static class LevelDefinition
+    {
+        public const string LevelName = "Closing Time Circuit";
+        public const int TotalScrap = 5;
+        public const int ScrapRequiredForFinish = 3;
+        public const int HazardCount = 3;
+        public const int EnemyCount = 1;
+        public const int MovingPlatformCount = 1;
+        public const int MagnetZoneCount = 1;
+        public const int CheckpointCount = 1;
+
+        public static bool IsStructurallyComplete()
+        {
+            return TotalScrap >= ScrapRequiredForFinish
+                && ScrapRequiredForFinish > 0
+                && HazardCount > 0
+                && EnemyCount > 0
+                && MovingPlatformCount > 0
+                && MagnetZoneCount > 0
+                && CheckpointCount > 0;
+        }
+    }
+}
