a95f5a4bc42d4a370c1ed246bcdc3b1cd8359bc4
Wojciech <swirjestem@hotmail.com>
2026-09-24 19:37:28 +0200

    SCRAP DASH: wire Level 1 lifecycle and checkpoint state

---
+++ games/001_scrap_dash/Source/ScrapDash/ScrapDashGameMode.cpp
@@ -0,0 +1,77 @@
+#include "ScrapDashGameMode.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "ScrapDashActors.h"
+#include "ScrapDashCharacter.h"
+#include "ScrapDashHUD.h"
+
+AScrapDashGameMode::AScrapDashGameMode()
+{
+    DefaultPawnClass = AScrapDashCharacter::StaticClass();
+    HUDClass = AScrapDashHUD::StaticClass();
+}
+
+void AScrapDashGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    FActorSpawnParameters Params;
+    Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+    GetWorld()->SpawnActor<AScrapDashLevelDirector>(
+        AScrapDashLevelDirector::StaticClass(), FVector::ZeroVector, FRotator::ZeroRotator, Params);
+
+    if (APlayerController* PC = GetWorld()->GetFirstPlayerController())
+    {
+        if (AScrapDashCharacter* Player = Cast<AScrapDashCharacter>(PC->GetPawn()))
+        {
+            Player->RespawnAt(CheckpointLocation);
+        }
+    }
+}
+
+void AScrapDashGameMode::RegisterScrap()
+{
+    ++TotalScrap;
+}
+
+void AScrapDashGameMode::CollectScrap()
+{
+    CollectedScrap = FMath::Clamp(CollectedScrap + 1, 0, TotalScrap);
+}
+
+void AScrapDashGameMode::SetCheckpoint(const FVector& WorldLocation)
+{
+    CheckpointLocation = WorldLocation;
+}
+
+void AScrapDashGameMode::RespawnPlayer(AScrapDashCharacter* Player)
+{
+    if (!Player || bLevelComplete)
+    {
+        return;
+    }
+
+    ++DeathCount;
+    Player->RespawnAt(CheckpointLocation);
+}
+
+bool AScrapDashGameMode::TryCompleteLevel()
+{
+    if (bLevelComplete || TotalScrap <= 0 || CollectedScrap < TotalScrap)
+    {
+        return false;
+    }
+
+    bLevelComplete = true;
+
+    if (APlayerController* PC = GetWorld()->GetFirstPlayerController())
+    {
+        if (AScrapDashCharacter* Player = Cast<AScrapDashCharacter>(PC->GetPawn()))
+        {
+            Player->GetCharacterMovement()->DisableMovement();
+        }
+    }
+
+    return true;
+}
